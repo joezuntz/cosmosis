1d48f8943d04cbb3b6a9697458871852f30f800e
Joe Zuntz <joezuntz@googlemail.com>
2014-03-10 16:17:19 +0000

    Include new errors from C into python

--- cosmosis/cosmosis_py/errors.py
+++ cosmosis/cosmosis_py/errors.py
@@ -1,20 +1,44 @@
 import collections
 
-DBS_SUCCESS = 0
-DBS_DATABLOCK_NULL = 1
-DBS_SECTION_NAME_NULL = 2
-DBS_SECTION_NOT_FOUND = 3
-DBS_NAME_NULL = 4
-DBS_NAME_NOT_FOUND = 5
-DBS_NAME_ALREADY_EXISTS = 6
-DBS_VALUE_NULL = 7
-DBS_WRONG_VALUE_TYPE = 8
-DBS_MEMORY_ALLOC_FAILURE = 9
+enum_names = [
+"DBS_SUCCESS",
+"DBS_DATABLOCK_NULL",
+"DBS_SECTION_NULL",
+"DBS_SECTION_NOT_FOUND",
+"DBS_NAME_NULL",
+"DBS_NAME_NOT_FOUND",
+"DBS_NAME_ALREADY_EXISTS",
+"DBS_VALUE_NULL",
+"DBS_WRONG_VALUE_TYPE",
+"DBS_MEMORY_ALLOC_FAILURE",
+"DBS_SIZE_NULL",
+"DBS_SIZE_NEGATIVE",
+"DBS_SIZE_INSUFFICIENT",
+"DBS_LOGIC_ERROR",
+]
 
-ERROR_MESSAGES = collections.defaultdict(lambda:  "{status}: Uknown error; probably cosmosis internal failure.  Please contact cosmosis team")
+
+class EnumGenerator(object):
+	"""This is a slightly insane hack to generate an enum into the local
+	namespace from a list of names.  This avoids a nested namespace.
+	Once the list of errors has settled down so we do not need to keep
+	retyping we can just hard-code them.
+	"""
+	def __init__(self, namespace):
+		self.namespace=namespace
+		self.value = 0
+	def add(self, name):
+		self.namespace[name] = self.value
+		self.value += 1
+
+enum = EnumGenerator(locals())
+for name in enum_names:
+	enum.add(name)
+
+ERROR_MESSAGES = collections.defaultdict(lambda:  "{status}: Uknown error; possibly cosmosis internal failure.  Please contact cosmosis team")
 ERROR_MESSAGES.update({
 	DBS_DATABLOCK_NULL: "{status}: Null (zero) datablock passed into function (section was {section}, name was {name})",
-	DBS_SECTION_NAME_NULL: "{status}: Null name passed into function (section was {section}",
+	DBS_SECTION_NULL: "{status}: Null name passed into function (section was {section}",
 	DBS_SECTION_NOT_FOUND: "{status}: Could not find section called {section} (name was {name})",
 	DBS_NAME_NULL: "{status}: Null name passed into function (section was {section})",
 	DBS_NAME_NOT_FOUND: "{status}: Could not find name {name} in section {section}",
@@ -22,6 +46,10 @@ ERROR_MESSAGES.update({
 	DBS_VALUE_NULL: "{status}: Passed a null value into function (section was {section}, name was {name})",
 	DBS_WRONG_VALUE_TYPE: "{status}: Tried to overwrite {name} in section {section} with a value of the wrong type",
 	DBS_MEMORY_ALLOC_FAILURE: "{status}: Failed to allocate memory for {name} in section {section}.",
+	DBS_SIZE_NULL: "{status}: Null parameter for returned size passed into function (section was {section}, name was {name})",
+	DBS_SIZE_NEGATIVE: "{status}: Negative maximum size passed into function (section was {section}, name was {name}",
+	DBS_SIZE_INSUFFICIENT: "{status}: Size of passed in array not large enough for what is needed (section was {section}, name was {name}",
+	DBS_LOGIC_ERROR: "{status}: Internal cosmosis logical error.  Please contact cosmosis team (section was {section}, name was {name})",
 })
 
 class BlockError(Exception):
