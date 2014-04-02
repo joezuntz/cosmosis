b552c84214c89c406103a34a9f684403fcbccb45
Douglas Rudd <drudd@uchicago.edu>
2014-04-02 14:33:04 -0500

    Added metaclass support for samplers and rough outline of parallel sampler interface (will definitely change)

--- src/runtime/sampler.py
+++ src/runtime/sampler.py
@@ -1,6 +1,19 @@
 import config
 
+sampler_registry = {}
+
 class Sampler(object):
+    class __metaclass__(type):
+        def __init__(cls, name, b, d):
+            type.__init__(cls, name, b, d)
+            if name in ["Sampler","ParallelSampler"]:
+                return
+            if not name.endswith("Sampler"):
+                raise ValueError("Sampler classes must be named [Name]Sampler")
+            config_name = name.rstrip("Sampler").lower()
+            sampler_registry[config_name] = cls
+            print "Registering: %s as %s" % (config_name, name)
+    
     def __init__(self, ini, pipeline):
         self.ini = ini
         self.pipeline = pipeline
@@ -15,3 +28,22 @@ class Sampler(object):
 
     def is_converged(self):
         return False
+
+
+class ParallelSampler(Sampler):
+    def __init__(self, ini, pipeline, pool=None):
+        Sampler.__init__(self, ini, pipeline)
+        self.pool = pool
+
+    def execute(self):
+        if self.pool:
+            self.pool.map()
+
+        else:
+            self.worker()
+
+    def worker(self):
+        raise NotImplementedError
+
+    def is_master(self):
+        return self.pool.is_master()
