d90ee68e128267ba123c00b5a5a0d9eabf544411
Joe Zuntz <joezuntz@googlemail.com>
2014-04-02 19:20:40 +0100

    update tests for c level

--- src/datablock/test_c_datablock_scalars.h
+++ src/datablock/test_c_datablock_scalars.h
@@ -6,6 +6,100 @@
 #include <stdlib.h>
 #include <string.h>
 
+void test_scalar_bool()
+{
+  printf("In test_scalar_bool\n");
+  typedef char* string;
+  c_datablock* s = make_c_datablock();
+  assert(s);
+
+  /* Get with a default returns the supplied default when no such
+     parameter is found. */
+  string section_name = "x";
+  bool val = false;
+  assert(c_datablock_get_bool_default(s, section_name, "no_such_param", true, &val) == DBS_SUCCESS);
+  assert(val == true);
+  assert(c_datablock_get_bool_default(s, section_name, "no_such_param", false, &val) == DBS_SUCCESS);
+  assert(val == false);
+
+  bool expected = true;
+
+  /* Put with no previous value should save the right value. */
+  assert(c_datablock_put_bool(s, section_name, "param_1", expected) == DBS_SUCCESS);
+  assert(c_datablock_get_bool(s, section_name, "param_1", &val) == DBS_SUCCESS);
+  assert(val == expected);
+  assert(c_datablock_get_bool_default(s, section_name, "param_1", 0, &val) == DBS_SUCCESS);
+  assert(val == expected);
+
+  /* Put of the same name boolo a different section should not collide. */
+  assert(c_datablock_put_bool(s, "y", "param_1", expected) == DBS_SUCCESS);
+  assert(c_datablock_get_bool(s, "y", "param_1", &val) == DBS_SUCCESS);
+  assert(val == expected);
+
+  /* Second put of the same name with same type should fail, and the
+     value should be unaltered. */
+  assert(c_datablock_put_bool(s, section_name, "param_1", -4) == DBS_NAME_ALREADY_EXISTS);
+  val = false;
+  assert(c_datablock_get_bool(s, section_name, "param_1", &val) == DBS_SUCCESS);
+  assert(val == expected);
+
+  /* Second put of the same name with different type should fail, and
+     the value should be unaltered. */
+  
+  assert(c_datablock_put_double(s, section_name, "param_1", 2.5) == DBS_NAME_ALREADY_EXISTS);
+  val = true;
+  assert(c_datablock_get_bool(s, section_name, "param_1", &val) == DBS_SUCCESS);
+  assert(val == expected);
+  
+  assert(c_datablock_put_string(s, section_name, "param_1", "wombat") == DBS_NAME_ALREADY_EXISTS);
+  val = false;
+  assert(c_datablock_get_bool(s, section_name, "param_1", &val) == DBS_SUCCESS);
+  assert(val == expected);
+  
+  assert(c_datablock_put_complex(s, section_name, "param_1", 3.5-2.5*_Complex_I) == DBS_NAME_ALREADY_EXISTS);
+  val = true;
+  assert(c_datablock_get_bool(s, section_name, "param_1", &val) == DBS_SUCCESS);
+  assert(val == expected);
+  
+
+  /* Replacement of an existing value with one of the same type should
+     save the right value. */
+  bool new_expected = false;
+  val = true;
+  assert(c_datablock_replace_bool(s, section_name, "param_1", new_expected) == DBS_SUCCESS);
+  assert(c_datablock_get_bool(s, section_name, "param_1", &val) == DBS_SUCCESS);
+  assert(val == new_expected);
+
+  /* Attempted replacement using a new name should not succeed, and
+     no parameter should be stored. */
+  assert(c_datablock_replace_bool(s, section_name, "no such parameter", false) == DBS_NAME_NOT_FOUND);
+  val = true;
+  assert(c_datablock_get_bool(s, section_name, "no such parameter", &val) == DBS_NAME_NOT_FOUND);
+  assert(val == true);
+
+  /* Attempted replacement using a name associated with a different
+     type should not succeed, and the stored value should not be
+     changed. */
+  assert(c_datablock_put_bool(s, section_name, "a value", true) == DBS_SUCCESS);
+  bool a_value;
+  
+  assert(c_datablock_replace_double(s, section_name, "a value", 2.5) == DBS_WRONG_VALUE_TYPE);
+  assert(c_datablock_get_bool(s, section_name, "a value", &a_value) == DBS_SUCCESS);
+  assert(a_value == true);
+  
+  assert(c_datablock_replace_string(s, section_name, "a value", "wombat") == DBS_WRONG_VALUE_TYPE);
+  assert(c_datablock_get_bool(s, section_name, "a value", &a_value) == DBS_SUCCESS);
+  assert(a_value == true);
+  
+  assert(c_datablock_replace_complex(s, section_name, "a value", 3.5-2.5*_Complex_I) == DBS_WRONG_VALUE_TYPE);
+  assert(c_datablock_get_bool(s, section_name, "a value", &a_value) == DBS_SUCCESS);
+  assert(a_value == true);
+  
+
+  destroy_c_datablock(s);
+
+}
+
 void test_scalar_int()
 {
   printf("In test_scalar_int\n");
@@ -96,13 +190,8 @@ void test_scalar_int()
 
   destroy_c_datablock(s);
 }
-#include "c_datablock.h"
 
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 void test_scalar_double()
 {
@@ -194,13 +283,7 @@ void test_scalar_double()
 
   destroy_c_datablock(s);
 }
-#include "c_datablock.h"
 
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 void test_scalar_complex()
 {
@@ -292,13 +375,7 @@ void test_scalar_complex()
 
   destroy_c_datablock(s);
 }
-#include "c_datablock.h"
 
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 void test_scalar_string()
 {
