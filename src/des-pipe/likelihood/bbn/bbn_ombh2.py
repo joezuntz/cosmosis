c634cd323096f6c58db606b22f4fc459a1577a93
Joe Zuntz <joezuntz@googlemail.com>
2014-04-03 22:31:53 +0100

    update BBN to cosmosis API

--- src/des-pipe/likelihood/bbn/bbn_ombh2.py
+++ src/des-pipe/likelihood/bbn/bbn_ombh2.py
@@ -1,38 +1,34 @@
-import pydesglue
 from numpy import log, pi
+import cosmosis
+
+#Should load these from cosmosis
+cosmo_section = "cosmological_parameters"
+like_section = "likelihoods"
 
 #Steigman 2008, equation 9
 BBN_OMBH2_MEAN = 0.0218
 BBN_OMBH2_SIGMA = 0.0011
 
-def setup(handle):
-	options = pydesglue.DesOptionPackage(handle)
-	section = options._default_section
+def setup(options):
 	mean = options.get_float(section, "mean", default=BBN_OMBH2_MEAN)
-	sigma = options.get_float(section, "sigma", default=BBN_OMBH2_SIGMA)
+	sigma = options.get_float(section, "sigma", default=BBN_OMBH2_MEAN)
 	norm = 0.5*log(2*pi*sigma**2)
-	return (mean,sigma,norm)
+	return (mean, sigma, norm)
 
-def execute(handle, config):
+
+def execute(block, config):
+	# Configuration data, read from ini file above
 	mean,sigma,norm = config
-	# Load the value of H0 from the package
-	try:
-		package = pydesglue.DesDataPackage.from_fits_handle(handle)
-		cosmo = pydesglue.section_names.cosmological_parameters
-		h0 = package.get_param(cosmo, 'H0')
-		omega_b = package.get_param(cosmo, 'OMEGA_B')
-		ombh2 = omega_b * h0**2
-		#compute the likelihood - just a simple Gaussian here
-		like = -(ombh2-mean)**2/sigma**2/2.0 - norm
-	
-		section = pydesglue.section_names.likelihoods
-		package.set_param(section, "BBN_LIKE", like)
-		package.write_to_fits_handle(handle)
-	except KeyboardInterrupt:
-		#If the user presses Ctrl-C we respect that
-		raise KeyboardInterrupt
-	except Exception as E:
-		#But any other kind of error is a problem
-		print "There was an error calculating the BBN likelihood: ", E
-		return 1
+
+	# Get parameters from sampler
+	h0 = block[cosmo, 'h0']
+	omega_b = block[cosmo, 'omega_b']
+
+	#compute the likelihood - just a simple Gaussian
+	ombh2 = omega_b * h0**2
+	like = -(ombh2-mean)**2/sigma**2/2.0 - norm
+	block[like_section, 'BBN_LIKE'] = like
+
+	#signal that everything went fine
 	return 0
+
