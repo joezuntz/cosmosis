8274ff4b6dec9e22e103e4e95785247ac5b04f4d
Joe Zuntz <joezuntz@googlemail.com>
2014-04-07 16:37:38 +0100

    Let test sampler save its output

--- src/samplers/test/test_sampler.py
+++ src/samplers/test/test_sampler.py
@@ -1,5 +1,8 @@
 from sampler import Sampler
 import numpy as np
+import os
+
+RUNTIME_INI_SECTION = "runtime"
 
 class TestSampler(Sampler):
     def config(self):
@@ -10,13 +13,19 @@ class TestSampler(Sampler):
         p = np.array([param.start for param in self.pipeline.varied_params])
         try:
             prior  = self.pipeline.prior(p)
-            like, extra = self.pipeline.likelihood(p)
+            like, extra, data = self.pipeline.likelihood(p, return_data=True)
+
             print "Prior      = ", prior
             print "Likelihood = ", like
             print "Posterior  = ", like+prior
         except Exception as e:
             print "(Could not get a likelihood) Error:"+str(e)
         self.converged = True
+
+        if self.ini.has_option(RUNTIME_INI_SECTION, "save_dir"):
+            output_data_dir = self.ini.get(RUNTIME_INI_SECTION, "save_dir")
+            if output_data_dir:
+                data.save_to_directory(output_data_dir, clobber=True)
 
     def is_converged(self):
         return self.converged
