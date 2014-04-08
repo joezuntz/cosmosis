57a393d9634b10e1a99a56e1e307da269da6d519
Joe Zuntz <joezuntz@googlemail.com>
2014-04-08 11:19:02 +0100

    added logging files

---
+++ src/datablock/datablock_logging.h
@@ -0,0 +1,30 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern const char * BLOCK_LOG_READ;
+extern const char * BLOCK_LOG_WRITE;
+extern const char * BLOCK_LOG_READ_FAIL;
+extern const char * BLOCK_LOG_WRITE_FAIL;
+extern const char * BLOCK_LOG_READ_DEFAULT;
+extern const char * BLOCK_LOG_REPLACE;
+extern const char * BLOCK_LOG_REPLACE_FAIL;
+extern const char * BLOCK_LOG_CLEAR;
+extern const char * BLOCK_LOG_DELETE;
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#ifdef __cplusplus
+#include <string>
+#include <typeindex>
+#include <typeinfo>
+namespace cosmosis
+{
+	typedef std::tuple<std::string,std::string,std::string,std::type_index> log_entry;
+}
+#endif
+
+
+
