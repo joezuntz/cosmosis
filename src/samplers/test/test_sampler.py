80165ce3682c567277534d4c3353b4ee09265eb6
Joe Zuntz <joezuntz@googlemail.com>
2014-04-08 15:46:28 +0100

    added a fatal-errors mode for test_sampler, and modified saving of output

--- src/samplers/test/test_sampler.py
+++ src/samplers/test/test_sampler.py
@@ -7,6 +7,7 @@ RUNTIME_INI_SECTION = "runtime"
 class TestSampler(Sampler):
     def config(self):
         self.converged = False
+        self.fatal_errors = self.ini.getboolean(RUNTIME_INI_SECTION, "fatal-errors", default=False)
 
     def execute(self):
         # load initial parameter values
@@ -19,13 +20,17 @@ class TestSampler(Sampler):
             print "Likelihood = ", like
             print "Posterior  = ", like+prior
         except Exception as e:
+            if self.fatal_errors: raise
             print "(Could not get a likelihood) Error:"+str(e)
+        try:
+            if self.ini.has_option(RUNTIME_INI_SECTION, "save_dir"):
+                output_data_dir = self.ini.get(RUNTIME_INI_SECTION, "save_dir")
+                if output_data_dir:
+                    data.save_to_directory(output_data_dir, clobber=True)
+        except Exception as e:
+            if self.fatal_errors: raise
+            print "Could not save output."
         self.converged = True
-
-        if self.ini.has_option(RUNTIME_INI_SECTION, "save_dir"):
-            output_data_dir = self.ini.get(RUNTIME_INI_SECTION, "save_dir")
-            if output_data_dir:
-                data.save_to_directory(output_data_dir, clobber=True)
 
     def is_converged(self):
         return self.converged
