f71661bcf8234dbbc053aa28ea9e7d82cc5dcbb0
Douglas Rudd <drudd@uchicago.edu>
2014-04-24 17:56:17 -0500

    Updated FFLAGS to include -J, removed -I from fortran compilation (moved from COMMON to COMMON_C)

--- config/compilers.mk
+++ config/compilers.mk
@@ -2,11 +2,10 @@ CXX=g++
 CC=gcc
 FC=gfortran
 
-COMMON_FLAGS=-O3 -g -fPIC -I${COSMOSIS_DIR}
-COMMON_C_FLAGS=$(COMMON_FLAGS) -Wall -Wextra -pedantic
+COMMON_FLAGS=-O3 -g -fPIC
+COMMON_C_FLAGS=$(COMMON_FLAGS) -I${COSMOSIS_DIR} -Wall -Wextra -pedantic
 CXXFLAGS=$(COMMON_C_FLAGS) $(USER_CXXFLAGS) -std=c++11
 CFLAGS=$(COMMON_C_FLAGS) $(USER_CFLAGS) -std=c99
-FFLAGS=$(COMMON_FLAGS) $(USER_FFLAGS) -std=gnu -fimplicit-none  -ffree-line-length-none
+FFLAGS=$(COMMON_FLAGS) -J${COSMOSIS_DIR}/cosmosis/datablock $(USER_FFLAGS) -std=gnu -fimplicit-none  -ffree-line-length-none
 LDFLAGS=$(USER_LDFLAGS) -L${COSMOSIS}/cosmosis/datablock
-
 PYTHON=python
