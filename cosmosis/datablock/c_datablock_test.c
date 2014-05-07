710b242c6a458c043b85bf22c2126fc92f79cf3b
Marc Paterno <paterno@fnal.gov>
2014-05-07 13:35:43 -0500

    Clean up formatting

--- cosmosis/datablock/c_datablock_test.c
+++ cosmosis/datablock/c_datablock_test.c
@@ -11,7 +11,6 @@
 void test_sections()
 {
   c_datablock* s = make_c_datablock();
-
   /* Null pointers should not cause crashes. */
   assert(c_datablock_has_section(NULL, NULL) == false);
   assert(c_datablock_has_section(s, NULL) == false);
@@ -19,14 +18,11 @@ void test_sections()
   assert(c_datablock_has_value(NULL, NULL, NULL) == false);
   assert(c_datablock_has_value(s, NULL, NULL) == false);
   assert(c_datablock_has_value(s, "boo", NULL) == false);
-
   assert(c_datablock_has_section(s, "cow") == false);
   assert(c_datablock_num_sections(s) == 0);
-
   assert(c_datablock_get_array_length(NULL, NULL, NULL) == -1);
   assert(c_datablock_get_array_length(s, NULL, NULL) == -1);
   assert(c_datablock_get_array_length(s, "no such section", NULL) == -1);
-
   /* Creating a parameter in a section must create the section. */
   assert(c_datablock_put_int(s, "s1", "a", 10) == DBS_SUCCESS);
   assert(c_datablock_has_section(s, "s1") == true);
@@ -34,14 +30,12 @@ void test_sections()
   assert(c_datablock_has_value(s, "s1", "a") == true);
   assert(c_datablock_has_value(s, "no such section", "a") == false);
   assert(c_datablock_has_value(s, "s1", "no such parameter") == false);
-
   /* Make a few more sections. */
   assert(c_datablock_put_int(s, "s2", "a", 10) == DBS_SUCCESS);
   assert(c_datablock_put_int(s, "s3", "a", 10) == DBS_SUCCESS);
   assert(c_datablock_put_double(s, "s4", "a", 10.5) == DBS_SUCCESS);
   const int expected_sections = 4;
   assert(c_datablock_num_sections(s) == expected_sections);
-
   /* Make sure all the section names are what we expect. We make the
      calls out-of-order intentionally.
    */
@@ -51,7 +45,6 @@ void test_sections()
   assert(strcmp(c_datablock_get_section_name(s, 1), "s2") == 0);
   assert(c_datablock_get_section_name(s, -1) == NULL);
   assert(c_datablock_get_section_name(s, 4) == NULL);
-
   destroy_c_datablock(s);
 }
 
@@ -68,24 +61,21 @@ void test_array_int()
 {
   c_datablock* s = make_c_datablock();
   assert(s);
-  int expected[] = {1,2,3};
-  int sz = sizeof(expected)/sizeof(int);
-
+  int expected[] = {1, 2, 3};
+  int sz = sizeof(expected) / sizeof(int);
   /* Put with no previous value should save the right value. */
   assert(c_datablock_put_int_array_1d(s, "x", "cow", expected, sz) == DBS_SUCCESS);
   int* val = NULL;
   int length;
-  assert(c_datablock_get_int_array_1d(s, "x", "cow", &val, &length ) == DBS_SUCCESS);
+  assert(c_datablock_get_int_array_1d(s, "x", "cow", &val, &length) == DBS_SUCCESS);
   assert(length == sz);
   TEST_ARRAY(length, val, expected);
   free(val);
   assert(c_datablock_get_array_length(s, "x", "cow") == sz);
-
   /* Get with preallocated buffer should return the right value. */
   const int big = 100;
   int buffer[big];
   assert(c_datablock_get_int_array_1d_preallocated(s, "x", "cow", buffer, &length, big) == DBS_SUCCESS);
-
   /* Get with a too-small buffer should fail, and leave the buffer
      untouched. The returned value of length will say how big the
      buffer needs to be. */
@@ -97,19 +87,16 @@ void test_array_int()
          DBS_SIZE_INSUFFICIENT);
   assert(too_small[0] == INT_MIN);
   assert(length == sz);
-
   /* Put of the same name into a different section should not collide. */
   assert(c_datablock_put_int_array_1d(s, "y", "cow", expected, sz) == DBS_SUCCESS);
   val = NULL;
-
-  assert(c_datablock_get_int_array_1d(s, "y", "cow", &val, &length ) == DBS_SUCCESS);
+  assert(c_datablock_get_int_array_1d(s, "y", "cow", &val, &length) == DBS_SUCCESS);
   TEST_ARRAY(length, val, expected);
   free(val);
-
   /* Second put of the same name with same type should fail, and the
      value should be unaltered. */
   int another_array[] = { 2, 3, 4, 5 };
-  const int another_sz = sizeof(another_array)/sizeof(int);
+  const int another_sz = sizeof(another_array) / sizeof(int);
   assert(c_datablock_put_int_array_1d(s, "x", "cow", another_array, another_sz) == DBS_NAME_ALREADY_EXISTS);
   val = NULL;
   length = -1;
@@ -118,7 +105,6 @@ void test_array_int()
   assert(length == sz);
   TEST_ARRAY(length, val, expected);
   free(val);
-
   /* Second put of the same name with different type should fail, and
      the value should be unaltered. */
   assert(c_datablock_put_int(s, "x", "cow", 2112) == DBS_NAME_ALREADY_EXISTS);
@@ -129,11 +115,10 @@ void test_array_int()
   assert(length == sz);
   TEST_ARRAY(length, val, expected);
   free(val);
-
   /* Replacement of an existing value with one of the same type should
      save the right value. */
   int new_expected[] = { 10, 20, 20, 10, 20, 10 };
-  const int new_sz = sizeof(new_expected)/sizeof(int);
+  const int new_sz = sizeof(new_expected) / sizeof(int);
   assert(c_datablock_replace_int_array_1d(s, "x", "cow", new_expected, new_sz) == DBS_SUCCESS);
   val = NULL;
   length = -1;
@@ -141,7 +126,6 @@ void test_array_int()
   assert(length == new_sz);
   TEST_ARRAY(length, val, new_expected);
   free(val);
-
   /* Attempted replacement using a new name should not succeed, and
      no parameter should be stored. */
   assert(c_datablock_replace_int_array_1d(s, "x", "no such parameter", new_expected, new_sz) == DBS_NAME_NOT_FOUND);
@@ -151,8 +135,6 @@ void test_array_int()
   assert(val == NULL);
   assert(length == -1);
   /* no need to free val, because nothing was allocated. */
-
-#if 0
   /* Attempted replacement using a name associated with a different
      type should not succeed, and the stored value should not be
      changed. */
@@ -161,87 +143,59 @@ void test_array_int()
   double d;
   assert(c_datablock_get_double(s, "x", "a double", &d) == DBS_SUCCESS);
   assert(d == 2.5);
-#endif
   destroy_c_datablock(s);
 }
 
-
-  /*
-  double x[] = {1,2,3};
-  c_datablock_put_double_array_1d(s, "pig", x, 3);
-  */
-
-  /*
-  double* y;
-  int sz;
-  c_datablock_get_double_array_1d(s, "pig", &y, &sz);
-  */
-
-  /*
-  double z[4];
-  int szz;
-  c_datablock_get_double_array_1d_preallocated(s, "pig", z, &szz, 4);
-  assert(szz == 3);
-  assert(z[0] == 1);
-  assert(z[1] == 2);
-  */
-
-void test_grid(){
+void test_grid()
+{
   c_datablock* s = make_c_datablock();
   printf("In test_grid\n");
-  DATABLOCK_STATUS status; 
-
+  DATABLOCK_STATUS status;
   int nx = 50;
   int ny = 100;
   double x[nx];
   double y[ny];
-  double **z = allocate_2d_double(nx, ny);
-
-  for (int i=0; i<nx; i++){
-    x[i] = 100.0 * i;
-  }
-
-  for (int i=0; i<ny; i++){
-    y[i] = -1.0 * i;
-  }
-
-  for (int i=0; i<nx; i++){
-   for (int j=0; j<ny; j++){
-    z[i][j] = x[i] + y[j];
-   }
-  }
-
-
+  double** z = allocate_2d_double(nx, ny);
+  for (int i = 0; i < nx; i++)
+    {
+      x[i] = 100.0 * i;
+    }
+  for (int i = 0; i < ny; i++)
+    {
+      y[i] = -1.0 * i;
+    }
+  for (int i = 0; i < nx; i++)
+    {
+      for (int j = 0; j < ny; j++)
+        {
+          z[i][j] = x[i] + y[j];
+        }
+    }
   status = c_datablock_put_double_grid(s, "test",
-  "X", nx, x,
-  "Y", ny, y,
-  "Z", z);
-
-  assert(status==0);
-
+                                       "X", nx, x,
+                                       "Y", ny, y,
+                                       "Z", z);
+  assert(status == 0);
   int na, nb;
-  double *a, *b, **c;
-
+  double* a, *b, **c;
   status =  c_datablock_get_double_grid(s, "test",
-  "X", &na, &a,
-  "Y", &nb, &b,
-  "Z", &c);
-  assert(status==0);
-
-  for (int i=0; i<nx; i++){
-   for (int j=0; j<ny; j++){
-    assert (c[i][j] == x[i] + y[j]);
-   }
-  }
+                                        "X", &na, &a,
+                                        "Y", &nb, &b,
+                                        "Z", &c);
+  assert(status == 0);
+  for (int i = 0; i < nx; i++)
+    {
+      for (int j = 0; j < ny; j++)
+        {
+          assert(c[i][j] == x[i] + y[j]);
+        }
+    }
   free(a);
   free(b);
   deallocate_2d_double(&z, nx);
   deallocate_2d_double(&c, na);
-
-  assert(status==0);
+  assert(status == 0);
   destroy_c_datablock(s);
-
-
 }
 
 
@@ -249,13 +203,11 @@ int main()
 {
   test_sections();
   test_grid();
-
   test_scalar_int();
   test_scalar_double();
   test_scalar_string();
   test_scalar_complex();
   test_scalar_bool();
-
   test_array_int();
   return 0;
 }
