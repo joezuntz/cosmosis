ba70823b0d193b085a7a7809a4f54646b6277be5
Joe Zuntz <joezuntz@googlemail.com>
2014-05-08 15:16:53 +0100

    Add a way to build -O0

--- config/compilers.mk
+++ config/compilers.mk
@@ -6,7 +6,12 @@ CXX=g++
 CC=gcc
 FC=gfortran
 
+ifeq (1,$(COSMOSIS_DEBUG))
+COMMON_FLAGS=-O0 -g -fPIC
+else
 COMMON_FLAGS=-O3 -g -fPIC
+endif
+
 COMMON_C_FLAGS=$(COMMON_FLAGS) -I${COSMOSIS_SRC_DIR}
 PEDANTIC_C_FLAGS=-Wall -Wextra -pedantic
 CXXFLAGS=$(COMMON_C_FLAGS) $(USER_CXXFLAGS) -std=c++11
