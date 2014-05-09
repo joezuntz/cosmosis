0a96c5036129a55daa67b3a75c76be865ee3ba46
Joe Zuntz <joezuntz@googlemail.com>
2014-05-09 17:56:47 +0100

    Add output of statistics to postprocess command

--- cosmosis/runtime/analytics.py
+++ cosmosis/runtime/analytics.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import sys
 import os
@@ -11,20 +12,52 @@ class Analytics(object):
         self.total_steps = 0
         self.means = np.zeros(len(params))
         self.m2 = np.zeros(len(params))
+        self.best_like = -np.inf
+        self.best_index = None
+        self.best_params = None
 
     def add_traces(self, traces):
         if traces.shape[1] != len(self.params):
             raise RuntimeError("The number of traces added to Diagnostics "
                                "does not match the number of varied "
                                "parameters!")
-
+        like_col = self.params.index("LIKE")
         num = float(self.total_steps)
-        for x in traces:
+        for i,x in enumerate(traces):
             num += 1.0
             delta = x - self.means
             self.means += delta/num
             self.m2 += delta*(x - self.means)
+            if like_col>-1:
+                like = x[like_col]
+                if like>self.best_like:
+                    self.best_like = like
+                    self.best_index = self.total_steps + i
+                    self.best_params = x
+
         self.total_steps += traces.shape[0]
+
+    @classmethod
+    def from_chain_files(cls, filenames, burn, thin):
+        if isinstance(filenames, str):
+            filenames = [filenames]
+
+        #currently only works on text files.
+        #TODO: rejig to use "load" methods on output objects
+        params = open(filenames[0]).readline().strip('#').split()
+
+        analytics = cls(params)
+        for filename in filenames:
+            chain = np.genfromtxt(filename).T
+            if burn<1:
+                nburn = len(chain[0]) * burn
+            else:
+                nburn = burn
+            chain = chain[:,nburn:]
+            if thin:
+                chain = chain[:,::thin]
+            analytics.add_traces(chain.T)
+        return analytics
 
     def trace_means(self):
         if self.pool:
