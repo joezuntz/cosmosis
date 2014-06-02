69d342eed215193ca09ac05aea45f61e29791dcc
Joe Zuntz <joezuntz@googlemail.com>
2014-06-02 13:54:55 +0100

    move around bits of plotting code ready for refactor

--- cosmosis/plotting/chain_plots.py
+++ cosmosis/plotting/chain_plots.py
@@ -1,6 +1,5 @@
 import matplotlib
 matplotlib.use('Agg')
-#matplotlib.rcParams['text.usetex']=True
 matplotlib.rcParams['font.family']='serif'
 matplotlib.rcParams['font.size']=18
 matplotlib.rcParams['legend.fontsize']=15
@@ -8,86 +7,30 @@ matplotlib.rcParams['xtick.major.size'] = 10.0
 matplotlib.rcParams['xtick.minor.size'] = 5.0
 matplotlib.rcParams['ytick.major.size'] = 10.0
 matplotlib.rcParams['ytick.minor.size'] = 5.0
+
 import pylab
 import scipy.stats
 import numpy as np
 import os
 import sys
-import ConfigParser
-import itertools
-import cmd
 import collections
+
 from .kde import KDE
-from .utils import NoSuchParameter, section_code
+from .plotter import Plotter
+
 import scipy.optimize
 try:
 	from cosmosis import output as output_module
 except ImportError:
 	print "Running without cosmosis: no pretty section names or running on ini files"
 
-class Plotter(object):
-	colors=['blue','red','green','cyan','gray']
-	def __init__(self, chain_data,latex_file=None, filetype="png", root_dir='.',prefix='',blind=False,**options):
-		self._chain_data = chain_data
-		all_names = set()
-		for chain_datum in self._chain_data.values():
-			for name in chain_datum.keys():
-				all_names.add(name)
-		self.all_names = sorted(list(all_names))
-		self.load_latex(latex_file)
-		self.nfile = len(chain_data)
-		self.filetype=filetype
-		self.options=options
-		self.root_dir = root_dir
-		self.prefix = prefix
-		self.blind = blind
-		if self.prefix and not self.prefix.endswith('_'):
-			self.prefix = self.prefix + "_"
-
-	def command(self, command, *args, **kwargs):
-		cmd = command.format(*args, **kwargs) + '\n'
-		self._output_file.write(cmd)
-
-	def blind_axes(self):
-		pylab.tick_params(
-		    axis='both',          # changes apply to the x-axis
-		    which='both',      # both major and minor ticks are affected
-		    bottom='off',      # ticks along the bottom edge are off
-		    top='off',         # ticks along the top edge are off
-		    left='off',      # ticks along the bottom edge are off
-		    right='off',         # ticks along the top edge are off
-		    labelbottom='off', # labels along the bottom edge are off
-		    labeltop='off', # labels along the bottom edge are off
-		    labelleft='off', # labels along the bottom edge are off
-		    labelright='off') # labels along the bottom edge are off
-
-
-	def load_latex(self, latex_file):
-		self._display_names = {}
-		if latex_file is not None:
-			latex_names = ConfigParser.ConfigParser()
-			latex_names.read(latex_file)
-		for i,col_name in enumerate(self.all_names):
-			display_name=col_name
-			if '--' in col_name:
-				section,name = col_name.lower().split('--')
-				try:
-					display_name = latex_names.get(section,name)
-				except ConfigParser.NoSectionError:
-					section = section_code(section)
-				except ConfigParser.NoOptionError:
-					pass					
-				try:
-					display_name = latex_names.get(section,name)
-				except:
-					pass
-			else:
-				if col_name in ["LIKE","likelihood"]:
-					display_name=r"{\cal L}"
-			self._display_names[col_name]=display_name
+class ChainPlotter(Plotter):
 
 	@classmethod
-	def from_outputs(cls, options, burn, thin, **kw):
+	def from_outputs(cls, options, **kw):
+		burn = kw.pop("burn", 0)
+		thin = kw.pop("thin", 1)
+		
 		column_names, chains, metadata, comments, final_metadata = output_module.input_from_options(options)
 
 		if burn==0:
@@ -131,27 +74,6 @@ class Plotter(object):
 			chain_datum = dict(zip(names,data))
 			chain_data[filename] = chain_datum	
 		return cls(chain_data, **kw)
-
-	def cols_for_name(self, name):
-		cols = collections.OrderedDict()
-		for filename, chain_datum in self._chain_data.items():
-			if name in chain_datum.keys():
-				cols[filename] = chain_datum[name]
-		if not cols:
-			raise NoSuchParameter(name)
-		return cols
-
-	def parameter_range(self, name):
-		cols = self.cols_for_name(name)
-		xmin = 1e30
-		xmax = -1e30
-		for col in cols.values():
-			if col.min() < xmin: xmin=col.min()
-			if col.max() > xmax: xmax=col.max()
-		if xmin==1e30 or xmax==-1e30:
-			raise ValueError("Could not find col max/min - NaNs in chain?")
-		return xmin,xmax
-
 
 	def _plot_1d(self, name, xmin_input=None, xmax_input=None, n=100, factor=2.0):
 		for i,(filename, x) in enumerate(self.cols_for_name(name).items()):
@@ -278,12 +200,10 @@ class Plotter(object):
 	def plot_keywords_1d(self):
 		return dict(lw=5)
 
-
-class CosmologyPlotter(Plotter):
 	def w0_wa_plot(self):
 		print "Doing W0 - WA plot"
-		w_name  = 'COSMOPAR--W'
-		wa_name = 'COSMOPAR--WA'
+		w_name  = 'cosmological_parameters--w'
+		wa_name = 'cosmological_parameters--wa'
 		self._plot_2d(w_name, wa_name, fill=self.options.get('fill',True), factor=1.5)
 		w_min,w_max = self.parameter_range(w_name)
 		wa_min,wa_max = self.parameter_range(wa_name)
