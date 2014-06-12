98888b7eb7d86ac740d98adcec594f7b8c2b49c9
Marc Paterno <paterno@fnal.gov>
2014-06-11 20:42:11 -0500

    Adding R scripts for making analysis plots.

---
+++ cosmosis/plotting/install-r-packages.r
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+needed.packages <- c( "optparse"
+                    , "latticeExtra"
+                    , "reshape2"
+                    )
+install.packages(needed.packages)
