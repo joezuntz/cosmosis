f123b643eb54f5906c1d3acec64da808d8a7b670
Joe Zuntz <joezuntz@googlemail.com>
2014-07-01 14:28:51 +0100

    add ndim c function and tests

--- cosmosis/datablock/c_datablock_test.c
+++ cosmosis/datablock/c_datablock_test.c
@@ -166,7 +166,6 @@ void test_array_int()
 void test_grid()
 {
   c_datablock* s = make_c_datablock();
-  printf("In test_grid\n");
   DATABLOCK_STATUS status;
   int nx = 50;
   int ny = 100;
@@ -215,6 +214,68 @@ void test_grid()
   destroy_c_datablock(s);
 }
 
+void test_ndim(){
+  printf("In test_ndim\n");
+
+{  c_datablock* s = make_c_datablock();
+  DATABLOCK_STATUS status = 0;
+  int x[4*4*4], y[5*5], z[17];
+  int xsize[3], ysize[2], zsize[1];
+  xsize[0] = 4; xsize[1] = 4; xsize[2] = 4;
+  ysize[0] = 5; ysize[1] = 5;
+  zsize[0] = 17;
+
+  for (int i=0; i<4*4*4; i++) x[i] = 0;
+  for (int i=0; i<5*5; i++) y[i] = 0;
+  for (int i=0; i<17; i++) z[i] = 0;
+  status = c_datablock_put_int_array(s, "sec", "x", x, 3, xsize);
+  status = c_datablock_put_int_array(s, "sec", "y", y, 2, ysize);
+  status = c_datablock_put_int_array(s, "sec", "z", z, 1, zsize);
+  assert(status == 0);
+
+  int ndim = 0;
+  status |= c_datablock_get_array_ndim(s, "sec", "x", &ndim);
+  assert(ndim == 3);
+  status |= c_datablock_get_array_ndim(s, "sec", "y", &ndim);
+  assert(ndim == 2);
+  status |= c_datablock_get_array_ndim(s, "sec", "z", &ndim);
+  assert(ndim == 1);
+  assert(status == 0);
+
+  destroy_c_datablock(s);
+}
+{
+  c_datablock* s = make_c_datablock();
+  DATABLOCK_STATUS status = 0;
+  double x[4*4*4], y[5*5], z[17];
+  int xsize[3], ysize[2], zsize[1];
+  xsize[0] = 4; xsize[1] = 4; xsize[2] = 4;
+  ysize[0] = 5; ysize[1] = 5;
+  zsize[0] = 17;
+
+  for (int i=0; i<4*4*4; i++) x[i] = 0;
+  for (int i=0; i<5*5; i++) y[i] = 0;
+  for (int i=0; i<17; i++) z[i] = 0;
+  status = c_datablock_put_double_array(s, "sec", "x", x, 3, xsize);
+  status = c_datablock_put_double_array(s, "sec", "y", y, 2, ysize);
+  status = c_datablock_put_double_array(s, "sec", "z", z, 1, zsize);
+  assert(status == 0);
+
+  int ndim = 0;
+  status |= c_datablock_get_array_ndim(s, "sec", "x", &ndim);
+  assert(ndim == 3);
+  status |= c_datablock_get_array_ndim(s, "sec", "y", &ndim);
+  assert(ndim == 2);
+  status |= c_datablock_get_array_ndim(s, "sec", "z", &ndim);
+  assert(ndim == 1);
+  assert(status == 0);
+
+  destroy_c_datablock(s);
+
+}
+
+}
+
 
 int main()
 {
@@ -226,5 +287,6 @@ int main()
   test_scalar_complex();
   test_scalar_bool();
   test_array_int();
+  test_ndim();
   return 0;
 }
