c09967360fcf7cddfe6a8f126adf2be08feedd59
Joe Zuntz <joezuntz@googlemail.com>
2014-08-29 11:35:57 +0200

    twiddle tweak demo

--- demos/tweaks.py
+++ demos/tweaks.py
@@ -4,13 +4,14 @@ import pylab
 class AddTitle(Tweaks):
 	#This can be a single filename, like "matter_power",
 	#a list of filenames, like ["matter_power", "hubble"]
-	#or this special value, which means all plots are affected.
-	filename="all plots"
+	#or a special value (see MoreSpace below), 
+	#which means all plots are affected.
+	filename="matter_power"
 
 	def run(self):
 		#These commands are run on all the target plots.
 		#Let's add a title.
-		pylab.title("Demo 1")
+		pylab.title("Matter Power Spectra")
 
 #Any number of tweaks can be applied.
 #We could have put this in the same tweak 
