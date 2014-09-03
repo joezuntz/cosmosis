4d532403f6d05060ccffbe05a48ca35b471aea6b
Joe Zuntz <joezuntz@googlemail.com>
2014-09-03 16:02:30 +0100

    correct extra10 example

--- demos/extra10.py
+++ demos/extra10.py
@@ -4,7 +4,7 @@ See demo 9 for more info on this.
 
 from cosmosis.postprocessing import plots
 
-class ColorScatter(plots.MultinestColorScatterPlot):
+class ColorScatter(plots.MCMCColorScatterPlot):
     x_column = "cosmological_parameters--omega_m"
     y_column = "cosmological_parameters--h0"
     color_column = "cosmological_parameters--n_s"
