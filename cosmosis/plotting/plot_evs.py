8c556b3d9c3b209a334399d985634b8f66c24a28
Joe Zuntz <joezuntz@googlemail.com>
2014-11-19 15:45:54 +0000

    Tweak mass function demos and plotting

--- cosmosis/plotting/plot_evs.py
+++ cosmosis/plotting/plot_evs.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python
 import matplotlib
+
+#Set some plot options
 matplotlib.use("Agg")
+matplotlib.rcParams["font.family"] = "serif"
+matplotlib.rcParams["font.size"] = 14
+matplotlib.rcParams["xtick.major.size"] = 8
+matplotlib.rcParams["ytick.major.size"] = 8
+matplotlib.rcParams["xtick.minor.size"] = 4
+matplotlib.rcParams["ytick.minor.size"] = 4
+
+#Load libraries
 import pylab
 import numpy as np
 
-
-data = np.loadtxt("demo_output_massfunction/evs/logphi.txt").T
-log_like = data
-data = np.loadtxt("demo_output_massfunction/evs/m.txt").T
-m = data
+#Load in the cosmosis output files
+log_like = np.loadtxt("demo_output_massfunction/evs/logphi.txt").T
+m = np.loadtxt("demo_output_massfunction/evs/m.txt").T
 
 #convert to like from log-like and normalize
 like = np.exp(log_like)
 like /= like.max()
 
-#make plot
+#make the plot
 pylab.xscale("log")
-pylab.plot(m/0.70799558006015664, like) # mass in M_sun
-pylab.xlabel("Max Mass (M_sun)")
-pylab.ylabel("Likelihood")
+pylab.plot(m/0.70799558006015664, like, lw=2) # mass in M_sun
 
+#Add a vertical bar for the observation:
 #XXMU J0044  Santos et al. 2011
-pylab.plot([4.02E14,4.02E14],[0.0,1.0],label="XXMU J0044 z = 1.579",linestyle='-.',color="red")
+pylab.plot([4.02E14,4.02E14],[0.0,1.0],
+	label="XXMU J0044 z = 1.579", linestyle='--', lw=2, color="red")
+
+#Set the axis and display setup
+pylab.xlabel("Maximum Mass ($M_\odot$)")
+pylab.ylabel("Likelihood")
+pylab.minorticks_on()
 pylab.legend()
 pylab.grid()
 
-#Save
+#Save the file
 pylab.savefig("maxmass.png")
