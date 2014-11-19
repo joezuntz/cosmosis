ef9e2e7b3d8b3aa7100ecfbb2844ac721c2bf718
Elise Jennings <ejennings@kicp.uchicago.edu>
2014-11-19 17:21:47 +0000

    changed filename

--- cosmosis/plotting/plot_evs.py
+++ cosmosis/plotting/plot_evs.py
@@ -15,8 +15,8 @@ import pylab
 import numpy as np
 
 #Load in the cosmosis output files
-log_like = np.loadtxt("demo_output_massfunction/evs/logphi.txt").T
-m = np.loadtxt("demo_output_massfunction/evs/m.txt").T
+log_like = np.loadtxt("demo13_output/evs/logphi.txt").T
+m = np.loadtxt("demo13_output/evs/m.txt").T
 
 #convert to like from log-like and normalize
 like = np.exp(log_like)
