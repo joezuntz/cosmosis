deb0113ea48caea7ece4ecdb206ed4edf41c31d5
Joe Zuntz <joezuntz@googlemail.com>
2015-04-28 15:49:14 +0100

    fix up and update importance sampler.  now runs okay

--- cosmosis/samplers/importance/importance_sampler.py
+++ cosmosis/samplers/importance/importance_sampler.py
@@ -1,4 +1,5 @@
 import itertools
+import collections
 import numpy as np
 
 from .. import ParallelSampler
@@ -12,35 +13,43 @@ def task(p):
 
 
 class ImportanceSampler(ParallelSampler):
+    #there are a bunch of these really, but we 
+    #need to add them manually so that we can
+    #keep the likelihood column at the end
+    #so the postprocessors don't need to be re-written
+    sampler_outputs = []
+    parallel_output = False
 
     def config(self):
         global importance_pipeline
         importance_pipeline = self.pipeline
-        self.input_filename = self.ini.get(INI_SECTION, "input-file")
+        self.input_filename = self.ini.get(INI_SECTION, "input")
         self.nstep = self.ini.getint(INI_SECTION, "nstep", 128)
         self.converged = False
         if self.is_master():
             self.load_samples(self.input_filename)
 
     def load_samples(self, filename):
-        col_names, cols, metadata, final_metadata = self.output.__class__.load(filename)
+        options = {"filename":filename}
+        col_names, cols, metadata, comments, final_metadata = self.output.__class__.load_from_options(options)
         # pull out the "like" column first
         col_names = [name.lower() for name in col_names]
         likelihood_index = col_names.index('like')
         if likelihood_index<0:
             raise ValueError("I could not find a 'like' column in the chain %s"%filename)
-        self.original_likelihoods = cols[likelihood_index]
+        self.original_likelihoods = cols[0].T[likelihood_index]
 
         #We split the parameters into three groups:
         #   - ones that we have listed as varying
         #   - one that are fixed - WHAT SHOULD WE DO ABOUT THESE???
         #   - extras ones - these should be saved and put in the output
 
-        self.original_extras = {}
+        self.original_extras = collections.OrderedDict()
         self.samples = []
         self.varied_params = []
         self.number_samples = len(cols[0])
-        for code,col in zip(col_names, cols):
+        print "Have %d samples from old chain." % self.number_samples
+        for code,col in zip(col_names, cols[0].T):
             #we have already handled the likelihood
             if code=='like':continue
             #parse the header names in to (section,name)
@@ -50,26 +59,37 @@ class ImportanceSampler(ParallelSampler):
                 #No parameter should be varied in the old chain
                 #but listed as fixed here
                 if (section,name) in self.pipeline.fixed_params:
-                    raise ValueError("A parameter %s varied in the old chain "
-                        "is listed as fixed in the new one"%code)
+                    raise ValueError("A parameter %s that was varied in the old chain "
+                        "is listed as fixed in the new one. Please set parameter limits on it."%code)
                 if (section,name) in self.pipeline.varied_params:
                     #Record the values of this parameter for later importance
                     #sampling
+                    print "Found column in both pipelines:", code
                     self.samples.append(col)
                     self.varied_params.append(bits)
                 else:
+                    print "Found column just in old pipeline:", code
                     #This parameter was varied in the old code but is not
                     #here.  So we just save it for output
                     self.original_extras[code] = col
+                    self.output.add_column(code, float)
             # anything here must be a sampler-specific 
             else:
+                print "Found non-parameter column:", code
                 self.original_extras[code] = col
+                self.output.add_column(code, float)
+
+        #Now finally add our actual two sampler outputs, old_like and like
+        #P is the original likelihood in the old chain we have samples from
+        #P' is the new likelihood.
+        self.output.add_column("old_like", float) #This is the old likelihood, log(P)
+        self.output.add_column("like", float) #This is the new likelihood, log(P')
+
 
         #Now we need to reorder the list to match the order our pipeline is expecting
         #If a parameter is not listed we use the starting value
         reordered_cols = []
         for p in self.pipeline.varied_params:
-            #This is the index of this parameter in the old chain
             i = self.varied_params.index(p)
             if i>=0:
                 col = self.samples[i]
@@ -94,12 +114,17 @@ class ImportanceSampler(ParallelSampler):
         else:
             results = map(task, samples_chunk)
 
+        #Collect together and output the results
         for i,(sample, (new_like, extra)) in enumerate(itertools.izip(samples_chunk, results)):
+            #We already (may) have some extra values from the pipeline
+            #as derived parameters.  Add to those any parameters used in the
+            #old pipeline but not the new one
+            for col in self.original_extras.values():
+                extra.append(col[start+i])
+            #and then the old and new likelihoods
             old_like = self.original_likelihoods[start+i]
-            importance = new_like - old_like
-            #print sample, new_like, extra
-            extra['importance'] = importance
-            extra['old_like'] = old_like
+            extra += [old_like,new_like]
+            #and save results
             self.output.parameters(sample, extra)
         #Update the current index
         self.current_index+=self.nstep
