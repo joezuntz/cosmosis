3bcc433c89a39d55e336e1ccbc1657519007dca6
Joe Zuntz <joezuntz@googlemail.com>
2015-08-05 14:01:33 +0100

    print out algorithm on failure, and save output ini

--- cosmosis/samplers/minuit/minuit_sampler.py
+++ cosmosis/samplers/minuit/minuit_sampler.py
@@ -37,6 +37,7 @@ class MinuitSampler(ParallelSampler):
         self.maxiter = self.read_ini("maxiter", int, 1000)
         self.save_dir = self.read_ini("save_dir", str, "")
         self.save_cov = self.read_ini("save_cov", str, "")
+        self.output_ini = self.read_ini("output_ini", str, "")
         self.verbose = self.read_ini("verbose", bool, False)
 
         #Minuit options
@@ -46,7 +47,7 @@ class MinuitSampler(ParallelSampler):
         self.width_estimate = self.read_ini("width_estimate", float, 0.1)
         self.tolerance = self.read_ini("tolerance", float, 0.01)
 
-        self.strategy = {
+        strategy = {
             "fast":0,
             "medium":1,
             "safe":2,
@@ -54,18 +55,20 @@ class MinuitSampler(ParallelSampler):
             "1":1,
             "2":2,
         }.get(self.strategy)
-        if self.strategy is None:
-            raise ValueError("Minuit 'strategy' parameter must be one of fast, medium, safe (default=medium)")
+        if strategy is None:
+            raise ValueError("Minuit 'strategy' parameter must be one of fast, medium, safe (default=medium) not '%s'"%self.strategy)
+        self.strategy = strategy
 
-        self.algorithm = {
+        algorithm = {
             "migrad":0,
             "fallback":1,
             "combined":1,
             "simplex":2,
             # "scan":3,
         }.get(self.algorithm)
-        if self.algorithm is None:
-            raise ValueError("Minuit 'algorithm' parameter must be one of migrad, fallback, simplex (default=migrad)")
+        if algorithm is None:
+            raise ValueError("Minuit 'algorithm' parameter must be one of migrad, fallback, simplex (default=migrad) not '%s'"%strategy)
+        self.algorithm = algorithm
 
         self.do_fisher = int(bool(fisher))
         self.fisher_file = fisher
@@ -83,7 +86,7 @@ class MinuitSampler(ParallelSampler):
             vector = np.frombuffer(cube_type.from_address(ct.addressof(cube_p.contents)))
             # vector = self.pipeline.denormalize_vector(cube_vector)
             try:
-                like, extra = self.pipeline.likelihood(vector)
+                like, extra = self.pipeline.posterior(vector)
             except KeyboardInterrupt:
                 sys.exit(1)
             if self.verbose:
@@ -102,6 +105,9 @@ class MinuitSampler(ParallelSampler):
             print
             print "Note that the # of function calls printed above is not the total count for all cores, just for one core."
             print
+
+        if self.output_ini:
+          self.pipeline.create_ini(param_vector, self.output_ini)
 
 
         for name, value in zip(param_names, param_vector):
