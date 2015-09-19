827b593e1c41c296d272fce6628df82e25fbceea
Elise Jennings <ejennings@midway-login1.rcc.local>
2015-09-19 15:16:18 -0500

    adding abc sampler and multigauss demo

--- cosmosis/samplers/abc/abc_sampler_mpi.py
+++ cosmosis/samplers/abc/abc_sampler_mpi.py
@@ -1,6 +1,7 @@
 from .. import ParallelSampler
 try:
     import abcpmc
+    from abcpmc import mpi_util
 except ImportError:
     raise ValueError("To use ABC PMC you need to install it with pip install abcpmc")
 import numpy as np
@@ -25,62 +26,68 @@ class ABCSampler(ParallelSampler):
         global abc_pipeline
         abc_pipeline = self.pipeline
 
-        self.threshold = self.read_ini("threshold",str, 'LinearEps')
-        self.epimax = self.read_ini('epimax', float,5.0)
-        self.epimin = self.read_ini('epimin',float, 1.0)
-        self.part_prop = self.read_ini("particle_prop",str,'weighted_cov')
-        self.set_prior = self.read_ini("set_prior",str,'Gaussian')
-        self.knn = self.read_ini("num_nn",int, 10)
-        self.npart = self.read_ini("npart",int,100)
-        self.niter = self.read_ini("niter",int,2)
-        self.ngauss = self.read_ini("ngauss",int,4)
-        self.run_multigauss = self.read_ini("run_multigauss",bool,False)
-        self.ndim = len(self.pipeline.varied_params)
 
-        #options for decreasing threshold
-        if self.threshold == 'ConstEps':
-            self.eps = abcpmc.ConstEps(self.niter, self.epimax)
-        elif self.threshold == 'ExpEps':
-            self.eps = abcpmc.ExponentialEps(self.niter, self.epimax,self.epimin)
-        else:
-            self.eps = abcpmc.LinearEps(self.niter, self.epimax, self.epimin)
+        if self.is_master():
+            if self.pool is not None:
+                raise ValueError("mpi ABC PMC not supported yet... ")
+                self.pool.close()
+            self.abcpmc = abcpmc
+            self.threshold = self.read_ini("threshold",str, 'LinearEps')
+            self.epimax = self.read_ini('epimax', float,5.0)
+            self.epimin = self.read_ini('epimin',float, 1.0)
+            self.part_prop = self.read_ini("particle_prop",str,'weighted_cov')
+            self.set_prior = self.read_ini("set_prior",str,'Gaussian')
+            self.knn = self.read_ini("num_nn",int, 10)
+            self.npart = self.read_ini("npart",int,100)
+            self.niter = self.read_ini("niter",int,2)
+            self.ngauss = self.read_ini("ngauss",int,4)
+            self.run_multigauss = self.read_ini("run_multigauss",bool,False)
+            self.ndim = len(self.pipeline.varied_params)
 
-        print "\nRunning ABC PMC"
-        print "with %d particles, %s prior, %s threshold, %d iterations over (%f,%f), %s kernal \n" % (self.npart,self.set_prior,self.threshold,self.niter,self.epimax,self.epimin,self.part_prop)
+            #options for decreasing threshold
+            if self.threshold == 'ConstEps':
+                self.eps = self.abcpmc.ConstEps(self.niter, self.epimax)
+            elif self.threshold == 'ExpEps':
+                self.eps = self.abcpmc.ExponentialEps(self.niter, self.epimax,self.epimin)
+            else:
+                self.eps = self.abcpmc.LinearEps(self.niter, self.epimax, self.epimin)
 
-        #Data file is read for use in dist() for each step
-        #parameter covariance used in the prior   
-        if self.run_multigauss:
-            self.sigma,self.data = self.generate_data(5000,0.25,1.)
-        else:
-            self.data = self.load_data()
-            self.sigma = self.load_covariance_matrix()
+            print "\nRunning ABC PMC"
+            print "with %d particles, %s prior, %s threshold, %d iterations over (%f,%f), %s kernal \n" % (self.npart,self.set_prior,self.threshold,self.niter,self.epimax,self.epimin,self.part_prop)
+
+            #Data file is read for use in dist() for each step
+            #parameter covariance used in the prior   
+            if self.run_multigauss:
+                self.sigma,self.data = self.generate_data(5000,0.25,1.)
+            else:
+                self.data = self.load_data()
+                self.sigma = self.load_covariance_matrix()
         
-        #At the moment the same prior (with variable hyperparameters) is
-        # used for all parameters  - would be nice to change this to be more flexible 
-        self.pmin = np.zeros(self.ndim)
-        self.pmax = np.zeros(self.ndim)
-        for i,pi in enumerate(self.pipeline.varied_params):
-            self.pmin[i] = pi.limits[0]
-            self.pmax[i] = pi.limits[1]
-        self.p0 = np.array([param.start for param in self.pipeline.varied_params])
-        if self.set_prior == 'uniform':
-            self.prior = abcpmc.TophatPrior(self.pmin,self.pmax)
-        else:
-            self.prior = abcpmc.GaussianPrior(self.p0, self.sigma*2) 
+            #At the moment the same prior (with variable hyperparameters) is
+            # used for all parameters  - would be nice to change this to be more flexible 
+            self.pmin = np.zeros(self.ndim)
+            self.pmax = np.zeros(self.ndim)
+            for i,pi in enumerate(self.pipeline.varied_params):
+                self.pmin[i] = pi.limits[0]
+                self.pmax[i] = pi.limits[1]
+            self.p0 = np.array([param.start for param in self.pipeline.varied_params])
+            if self.set_prior == 'uniform':
+                self.prior = self.abcpmc.TophatPrior(self.pmin,self.pmax)
+            else:
+                self.prior = self.abcpmc.GaussianPrior(self.p0, self.sigma*2) 
 
-        #create sampler
-        self.sampler = abcpmc.Sampler(N=self.npart, Y=self.data, postfn=abc_model, dist=self.dist)
+            #create sampler
+            self.sampler = self.abcpmc.Sampler(N=self.npart, Y=self.data, postfn=abc_model, dist=self.dist,pool=self.pool)
 
-        #set particle proposal kernal
-        abcpmc.Sampler.particle_proposal_kwargs = {}
-        if self.part_prop == 'KNN':
-            abcpmc.Sampler.particle_proposal_kwargs = {'k':self.knn}
-            self.sampler.particle_proposal_cls = abcpmc.KNNParticleProposal
-        elif self.part_prop == 'OLCM':
-            self.sampler.particle_proposal_cls = abcpmc.OLCMParticleProposal
+            #set particle proposal kernal
+            self.abcpmc.Sampler.particle_proposal_kwargs = {}
+            if self.part_prop == 'KNN':
+                self.abcpmc.Sampler.particle_proposal_kwargs = {'k':self.knn}
+                self.sampler.particle_proposal_cls = self.abcpmc.KNNParticleProposal
+            elif self.part_prop == 'OLCM':
+                self.sampler.particle_proposal_cls = self.abcpmc.OLCMParticleProposal
 
-        self.converged = False
+            self.converged = False
 
     def output_samples(self,out):
         for o in out:
