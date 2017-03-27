7dc7a69266a7c9027199950702add3e9b206a750
Joe Zuntz <joezuntz@googlemail.com>
2017-03-27 12:06:19 +0100

    Add an experimental fault handler

---
+++ cosmosis/runtime/handler.py
@@ -0,0 +1,16 @@
+import os
+
+dirname,_=os.path.split(__file__)
+libname=os.path.join(dirname,"experimental_fault_handler.so")
+
+def activate_experimental_fault_handling():
+    try:
+        import faulthandler
+    except ImportError:
+        raise ImportError("You must install the python faulthandler package to use experimental fault handling")
+    import ctypes
+    lib = ctypes.cdll.LoadLibrary(libname)
+    lib.enable_combined_segfault_handler.argtypes=[]
+    lib.enable_combined_segfault_handler.restype = None
+    lib.enable_combined_segfault_handler()
+    print("Experimental fault handling enabled")
