b3c49b57772b152a0190a177956e26ae639e5bdf
Richard Rollins <rprollins@users.noreply.bitbucket.org>
2017-09-26 13:44:49 +0100

    Fixed incorrect import statement in cosmosis/datablock/__init__.py

--- cosmosis/datablock/__init__.py
+++ cosmosis/datablock/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 from .cosmosis_py.block import DataBlock, BlockError, option_section, SectionOptions
-from . import cosmosis_py.section_names as names
+from .cosmosis_py import section_names as names
