c2250d1d2c6355deed0c1f24d638d4d774cf4eea
Joe Zuntz <joezuntz@googlemail.com>
2018-07-31 12:27:02 +0100

    update scripting example

--- cosmosis/tools/scripting-example.py
+++ cosmosis/tools/scripting-example.py
@@ -3,47 +3,50 @@ from builtins import str
 from cosmosis.runtime.config import Inifile
 from cosmosis.runtime.pipeline import LikelihoodPipeline
 import numpy as np
-import pylab
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
-#The easiest way to start a pipeline it from a parameter file.
-#You load in the file, and then build a LikelihoodPipeline from it.
-#You could modify things in the ini file object after loading 
-#if you wanted.
+# The easiest way to start a pipeline it from a parameter file.
 ini = Inifile("demos/demo15.ini")
+
+# You can modify things in the ini file object after loading.
+# In this case we will switch off some verbose output
+ini.set("angular_power", "verbose",  "F")
+
+
+# Make the pipeline itself
 pipeline = LikelihoodPipeline(ini)
 
-#You can modify which parameters you vary here.
-#In the next cosmosis version there will be a 
-#nicer method for doing this.
-for parameter in pipeline.parameters:
-    if parameter == ("cosmological_parameters", "omega_m"):
-        parameter.limits = (0.2, 0.4)
+# You can modify which parameters you vary now
+pipeline.set_varied("cosmological_parameters", "omega_m", 0.2, 0.4)
+pipeline.set_fixed("cosmological_parameters", "h0", 0.72)
 
-#You can also override these properties if useful
+# You can also override these properties if useful
 pipeline.quiet = True
 pipeline.debug = False
 pipeline.timing = False
 
-#Let's look through different values of omega_m
-#and get a Galaxy Galaxy-Lensing spectrum for each of them
+# Let's look through different values of omega_m
+# and get a Galaxy Galaxy-Lensing spectrum for each of them
 for omega_m in [0.2, 0.25, 0.3, 0.35, 0.4]:
 
-    #In this method of running the pipeline we
-    #pass it a value for each of the parameters 
-    #we have told it to vary.
-    #We could check what these are by looking at 
-    #pipeline.varied_params
+    # In this method of running the pipeline we
+    # pass it a value for each of the parameters 
+    # we have told it to vary.
+    # We could check what these are by looking at 
+    # pipeline.varied_params
     data = pipeline.run_parameters([omega_m])
 
-    #data is a DataBlock - can get things out of it as in any
-    #cosmosis module:
-    ell = data['ggl_cl', 'ell']
-    cl  = data['ggl_cl', 'bin_1_1']
+    # data is a DataBlock - can get things out of it as in any
+    # cosmosis module:
+    ell = data['galaxy_shear_cl', 'ell']
+    cl  = data['galaxy_shear_cl', 'bin_1_1']
 
-    #Make a plot for this value
-    pylab.loglog(ell, np.abs(cl), label=str(omega_m))
+    # Make a plot for this value
+    plt.loglog(ell, np.abs(cl), label=str(omega_m))
     print("Done ", omega_m)
 
-#Save our plot.
-pylab.legend()
-pylab.savefig("test_scripting.png")
+# Save our plot.
+plt.legend()
+plt.savefig("test_scripting.png")
