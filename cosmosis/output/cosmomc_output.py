801d463d98547b944c3d728d43d4924554f1a7f7
Joe Zuntz <joezuntz@googlemail.com>
2021-09-06 09:04:20 +0100

    import fix from old version

--- cosmosis/output/cosmomc_output.py
+++ cosmosis/output/cosmomc_output.py
@@ -22,7 +22,7 @@ class CosmoMCOutput(TextColumnOutput):
 
     def _close(self):
         self._write_parameters_multiplicity()
-        super(CosmoMCOutput, self)._close(self)
+        super(CosmoMCOutput, self)._close()
         if self._paramfile:
             self._paramfile.close()
 
