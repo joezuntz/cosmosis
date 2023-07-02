c6cc04badca111a764d387c6810b8c3dacc67f13
MinhLCTP <nguyenmn@umich.edu>
2023-07-01 22:08:55 -0400

    Update maxlike sampler to enable simulated annealing option through scipy.optimize.basinhoppping

--- cosmosis/samplers/maxlike/maxlike_sampler.py
+++ cosmosis/samplers/maxlike/maxlike_sampler.py
@@ -1,7 +1,6 @@
 from .. import Sampler
 import numpy as np
 
-
 class MaxlikeSampler(Sampler):
     sampler_outputs = [("prior", float), ("like", float), ("post", float)]
 
@@ -10,6 +9,18 @@ class MaxlikeSampler(Sampler):
         self.maxiter = self.read_ini("maxiter", int, 1000)
         self.output_ini = self.read_ini("output_ini", str, "")
         self.output_cov = self.read_ini("output_covmat", str, "")
+        self.annealing = self.read_ini("annealing", bool, False)
+        if self.annealing:
+            self.annealing_niter = self.read_ini("annealing_niter", int, 100)
+            self.annealing_temperature = self.read_ini("annealing_temperature", float, 1.0)
+            self.annealing_stepsize = self.read_ini("annealing_stepsize", float, 0.5)
+            self.annealing_take_step = None
+            self.annealing_accept_test = None
+            self.annealing_interval = self.read_ini("annealing_stepsize_update_interval", int, 50)
+            self.annealing_niter_success = self.read_ini("annealing_niter_success", int, self.annealing_niter+2)
+            self.annealing_seed = self.read_ini("annealing_seed", int, 1)
+            self.annealing_target_accept_rate = self.read_ini("annealing_target_accept_rate", float, 0.5)
+            self.annealing_stepwise_factor = self.read_ini("annealing_stepwise_factor", float, 0.9)
         self.method = self.read_ini("method",str,"Nelder-Mead")
         self.max_posterior = self.read_ini("max_posterior", bool, False)
 
@@ -31,6 +42,10 @@ class MaxlikeSampler(Sampler):
 
     def execute(self):
         import scipy.optimize
+
+        def print_basinhopping_log(x, f, accepted):
+            print("found minimum %.4f accepted %d" % (f, int(accepted)))
+            return True
 
         def likefn(p_in):
             #Check the normalization
@@ -55,9 +70,26 @@ class MaxlikeSampler(Sampler):
         if not np.isfinite(start_like):
             raise RuntimeError('invalid starting point for maxlike')
 
-        result = scipy.optimize.minimize(likefn, start_vector, method=self.method, 
-          jac=False, tol=self.tolerance,  #bounds=bounds, 
-          options={'maxiter':self.maxiter, 'disp':True})
+        if self.annealing:
+            minimizer_kwargs = {'method': self.method, 'jac': False, 'tol': self.tolerance, 'options': {'maxiter': self.maxiter}}
+            result = scipy.optimize.basinhopping(likefn, start_vector,
+            niter = self.annealing_niter,
+            T = self.annealing_temperature,
+            stepsize = self.annealing_stepsize,
+            minimizer_kwargs=minimizer_kwargs,
+            take_step = self.annealing_take_step,
+            accept_test = self.annealing_accept_test,
+            callback = print_basinhopping_log,
+            interval = self.annealing_interval,
+            disp=True,
+            niter_success = self.annealing_niter_success,
+            seed = self.annealing_seed,
+            target_accept_rate = self.annealing_target_accept_rate,
+            stepwise_factor = self.annealing_stepwise_factor)
+        else:
+            result = scipy.optimize.minimize(likefn, start_vector, method=self.method,
+            jac=False, tol=self.tolerance,  #bounds=bounds,
+            options={'maxiter':self.maxiter, 'disp':True})
 
         opt_norm = result.x
         opt = self.pipeline.denormalize_vector(opt_norm)
