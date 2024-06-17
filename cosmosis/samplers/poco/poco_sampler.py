f90c8044d83c45f6d060e532d484060a814a28be
Minas Karamanis <minaskar@gmail.com>
2024-06-17 10:54:40 +0200

    Added support for pocoMC 1.2.1

--- cosmosis/samplers/poco/poco_sampler.py
+++ cosmosis/samplers/poco/poco_sampler.py
@@ -11,14 +11,24 @@ def log_likelihood(p):
     return like
 
 
-def log_prior(p):
-    return pipeline.prior(p)
+class Prior:
+    def __init__(self):
+        self.dim = len(self.pipeline.varied_params)
+        self.bounds = np.array([p.limits for p in self.pipeline.varied_params])
+
+    def logpdf(self, x):
+        return np.array([self.pipeline.prior(p) for p in x])
+    
+    def rvs(self, size=None):
+        return np.array([self.pipeline.randomized_start() for i in range(size)])
 
 
-class PocoSampler(ParallelSampler):
+class PocoMCSampler(ParallelSampler):
     parallel_output = False
-    supports_resume = False
-    sampler_outputs = [("prior", float), ("post", float)]
+    supports_resume = True
+    #internal_resume = True
+    sampler_outputs = [('log_weight', float), ('prior', float),
+                       ("post", float)]
 
     def config(self):
         global pipeline
@@ -30,84 +40,59 @@ class PocoSampler(ParallelSampler):
             self.pocomc = pocomc
 
             # Parameters of the sampler
-            self.ndim = len(self.pipeline.varied_params)
-            self.nparticles = self.read_ini("n_particles", int)
-            self.nsample_add = self.read_ini("add_samples", int, 0)
-            self.threshold = self.read_ini("threshold", float, 1.0)
-            self.scale = self.read_ini("scale", bool, True)
-            self.rescale = self.read_ini("rescale", bool, False)
-            self.diagonal = self.read_ini("diagonal", bool, True)
-            self.ess = self.read_ini("ess", float, 0.95)
-            self.gamma = self.read_ini("gamma", float, 0.75)
-            self.n_min = self.read_ini("n_min", int, self.ndim // 2)
-            self.n_max = self.read_ini("n_max", int, self.ndim * 10)
+            self.n_effective = self.read_ini("n_effective", int, 512)
+            self.n_active = self.read_ini("n_active", int, 256)
+            self.flow = self.read_ini("flow", str, "nsf6")
+            self.precondition = self.read_ini("precondition", bool, True)
+            self.dynamic = self.read_ini("dynamic", bool, True)
+            self.output_dir = self.read_ini("output_dir", str, "output")            
             seed = self.read_ini("seed", int, 0)
             if seed == 0:
                 seed = None
 
-            # Starting positions and values for the chain
-            self.num_samples = 0
-            self.prob0 = None
-
-            self.p0 = np.array(
-                [self.pipeline.randomized_start() for i in range(self.nparticles)]
-            )
-            logs.overview(
-                "Generating random starting positions from within prior"
-            )
-
-            bounds = np.array([p.limits for p in self.pipeline.varied_params])
+            # Parameters of the run
+            self.n_total = self.read_ini("n_total", int, 4096)
+            self.n_evidence = self.read_ini("n_evidence", int, 4096)
+            self.progress = self.read_ini("progress", bool, True)
+            self.resume_state_path = self.read_ini("resume_state_path", str, None)
+            self.save_every = self.read_ini("save_every", int, None)
 
             # Finally we can create the sampler
             self.sampler = self.pocomc.Sampler(
-                n_dim=self.ndim,
-                n_particles=self.nparticles,
-                log_likelihood=log_likelihood,
-                log_prior=log_prior,
+                prior=Prior(),
+                likelihood=log_likelihood,
                 random_state=seed,
-                threshold=self.threshold,
-                scale=self.scale,
-                rescale=self.rescale,
-                diagonal=self.diagonal,
-                vectorize_likelihood=False,
-                vectorize_prior=False,
-                infer_vectorization=False,
+                n_effective=self.n_effective,
+                n_active=self.n_active,
+                flow=self.flow,
+                precondition=self.precondition,
+                dynamic=self.dynamic,
+                output_dir=self.output_dir,
                 pool=self.pool,
-                bounds=bounds,
+                blobs_dtype=float,
             )
 
             self.converged = False
 
-            for sec, name in pipeline.extra_saves:
-                col = "{}--{}".format(sec, name)
-                logs.warning(
-                    "WARNING: POCO DOES NOT SUPPORT DERIVED PARAMS - NOT SAVING {}".format(
-                        col
-                    )
-                )
-                self.output.del_column(col)
-
     def execute(self):
-        # There's no checkpointing yet
         self.sampler.run(
-            self.p0, n_min=self.n_min, n_max=self.n_max, gamma=self.gamma, ess=self.ess
+            n_total=self.n_total,
+            n_evidence=self.n_evidence,
+            progress=self.progress,
+            resume_state_path=self.resume_state_path,
+            save_every=self.save_every,
         )
 
-        if self.nsample_add > 0:
-            self.sampler.add_samples(self.nsample_add)
+        logz, logz_err = self.sampler.evidence()
 
-        results = self.sampler.results
+        for sample, logw, logl, logp, blob in zip(*self.sampler.posterior(return_blobs=True, return_logw=True)):
+            extra = np.atleast_1d(blob)
+            prior = logp
+            logpost = logl + prior
+            self.output.parameters(sample, extra, logw, prior, logpost)
 
-        samples = results["samples"]
-        likes = results["loglikelihood"]
-        priors = results["logprior"]
-        n = len(samples)
-        outputs = []
-        for (p, prior, like) in zip(samples, priors, likes):
-            post = prior + like
-            self.output.parameters(p, prior, post)
-
-        self.output.final("log_z", results["logz"][-1])
+        self.output.final("log_z", logz)
+        self.output.final("log_z_error", logz_err)
         self.converged = True
 
     def is_converged(self):
